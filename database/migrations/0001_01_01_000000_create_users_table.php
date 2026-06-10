f855814a0bd116632ab8db97297c1481b7b5c80b
V-vey <htayag.1228@gmail.com>
2026-06-10 20:09:44 +0800

    migration

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -14,9 +14,16 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
+            $table->string('email');
+            $table->string('contact');
             $table->string('password');
+            $table->enum('role', ['HR', 'employee']);
+            $table->string('position');
+            $table->date('joined_date');
+            $table->string('contract_type');
+            $table->string('gender');
+            $table->date('date_of_birth');
+            $table->text('address');
             $table->rememberToken();
             $table->timestamps();
         });
