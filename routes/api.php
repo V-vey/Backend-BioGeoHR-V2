b8a023f5e335b06435bfc68f3e7dc64a6e76fa05
V-vey <htayag.1228@gmail.com>
2026-06-14 02:54:18 +0800

    controller

--- routes/api.php
+++ routes/api.php
@@ -2,10 +2,12 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\UserController;
+use App\Http\Controllers\UsersController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-Route::apiResource('users', UserController::class);
+Route::apiResource('users', UsersController::class);
+Route::patch('/users/{id}', [UsersController::class, 'update']);
+Route::delete('/users/{id}', [UsersController::class, 'destroy']);
