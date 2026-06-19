423509238496aec199d757d1050feb90a341442a
V-vey <htayag.1228@gmail.com>
2026-06-19 18:09:20 +0800

    user location

---
+++ app/Models/UserLocation.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Notifications\Notifiable;
+
+class UserLocation extends Model
+{
+    use HasFactory, Notifiable;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var list<string>
+     */
+    protected $fillable = [
+        'user_id',
+        'location_id',
+        'attendance_id',
+        'latitude',
+        'longitude',
+    ];
+}
---
+++ database/migrations/2026_06_19_094229_users_locations.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('users_locations', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id');
+            $table->foreignId('location_id');
+            $table->foreignId('attendance_id');
+            $table->decimal('longitude');
+            $table->decimal('latitude');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('users_locations');
+    }
+};
